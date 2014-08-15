aed4ee93918823605d0a32d1686bcbf224571362
RaphaelDeLaGhetto <daniel@capitolhill.ca>
2014-08-15 11:16:38 -0600

    Get version 0.0.15

--- dist/gebo-client-token.js
+++ dist/gebo-client-token.js
@@ -1,3 +1,5 @@
+/*! gebo-client-token - v0.0.13 - 2014-08-15
+* Copyright (c) 2014 ; Licensed  */
 ;
 (function () {
   'use strict';
@@ -12,9 +14,23 @@
     '$resource',
     '$filter',
     function ($http, $q, $window, $rootScope, $resource, $filter) {
+      /**
+     * This is the data returned on verification.
+     */
       var _agent = {};
+      /**
+     *  This response type must be passed to the authorization endpoint using
+     *  the implicit grant flow (4.2.1 of RFC 6749).
+     */
       var RESPONSE_TYPE = 'token';
+      /**
+     * Create a special object for endpoint fields that are required and missing.
+     * If any endpoint items still contain it when Token is used, raise an error.
+     */
       var REQUIRED_AND_MISSING = {};
+      /**
+     * Default endpoint fields. These need to be set somewhere else.
+     */
       var _endpoint = {
           gebo: REQUIRED_AND_MISSING,
           redirect: REQUIRED_AND_MISSING,
@@ -24,9 +40,15 @@
           verify: '/verify',
           perform: '/perform',
           send: '/send',
+          logout: '/logout',
           localStorageName: REQUIRED_AND_MISSING,
           scopes: ''
         };
+      /**
+     * Get the token request parameters
+     *
+     * @throws Error
+     */
       var _getParams = function () {
         var requiredAndMissing = [];
         angular.forEach(_endpoint, function (value, key) {
@@ -45,19 +67,62 @@
           scope: _endpoint.scopes
         };
       };
+      /**
+     * Set the configuration options
+     */
       var _setEndpoints = function (endpoint) {
         _endpoint = angular.extend(_endpoint, endpoint);
       };
+      // TODO: get/set might want to support expiration to reauthenticate
+      // TODO: check for localStorage support and otherwise perhaps use other
+      // methods of storing data (e.g. cookie)
+      /**
+     * Returns the stored access token.
+     *
+     * @returns {string} The access token.
+     */
       var _get = function () {
         return localStorage.getItem(_endpoint.localStorageName);
       };
+      /**
+     * Persist the access token so that it can be retrieved later by.
+     *
+     * @param accessToken
+     */
       var _set = function (accessToken) {
         localStorage.setItem(_endpoint.localStorageName, accessToken);
       };
+      /**
+     * Remove token from local storage and clear
+     * authentication data
+     */
       var _clear = function () {
         _agent = {};
         localStorage.removeItem(_endpoint.localStorageName);
       };
+      /**
+     * Verifies an access token asynchronously.
+     *
+     * @param extraParams An access token received from the authorization server.
+     * @param popupOptions Settings for the display of the popup.
+     *
+     * @returns {Promise} Promise that will be resolved when the authorization
+     *                    server has verified that thetoken is valid, and we've
+     *                    verified that the token is passed back has audience
+     *                    that matches our client ID (to prevent the Confused
+     *                    Deputy Problem).
+     *
+     *  If there's an error verifying the token, the promise is rejected with an
+     *  object identifying the `name` error in the name member. The `name` can 
+     *  be either:
+     *
+     *    - `invalid_audience`: The audience didn't match our client ID.
+     *    - `error_response`: The server responded with an error, typically
+     *                        because the token was invalid.  In this
+     *                        case, the callback parameters to `error` callback
+     *                        on `$http` are available in the object (`data`,
+     *                        `status`, `headers`, `endpoint`).
+     */
       var _getTokenByPopup = function (extraParams, popupOptions) {
         popupOptions = angular.extend({
           name: 'AuthPopup',
@@ -81,6 +146,8 @@
           return pairs.join(',');
         };
         var popup = window.open(url, popupOptions.name, formatPopupOptions(popupOptions.openParams));
+        // TODO: binding occurs for each reauthentication,
+        // leading to leaks for long-running apps.
         angular.element($window).bind('message', function (event) {
           if (event.source === popup && event.origin === window.location.origin) {
             $rootScope.$apply(function () {
@@ -92,8 +159,24 @@
             });
           }
         });
+        // TODO: reject deferred if the popup was closed without
+        // a message being delivered + maybe offer a timeout
         return deferred.promise;
       };
+      /**
+     * Given an flat object, return an URL-friendly query string.  Note
+     * that for a given object, the return value may be.
+     *
+     * @example
+     * <pre>
+     *    // returns 'color=red&size=large'
+     *    _objectToQueryString({color: 'red', size: 'large'})
+     * </pre>
+     *
+     * @param {Object} A flat object containing keys for a query string.
+     * 
+     * @returns {string} An URL-friendly query string.
+     */
       var _objectToQueryString = function (obj) {
         var str = [];
         angular.forEach(obj, function (value, key) {
@@ -101,6 +184,13 @@
         });
         return str.join('&');
       };
+      /**
+     * Verify the user is still authenticated
+     *
+     * @param string
+     *
+     * @return promise
+     */
       var _verify = function (accessToken) {
         var deferred = $q.defer();
         $http.get(_getEndpointUri('verify') + '?access_token=' + accessToken).success(function (response) {
@@ -111,8 +201,17 @@
         });
         return deferred.promise;
       };
+      /**
+     * Attempt to perform an action on a resource
+     *
+     * You'd think this would be easy to DRY out...
+     *
+     * @param Object 
+     */
       function _perform(content) {
         var deferred = $q.defer();
+        // A perform message can either come as
+        // a FormData object or plain JSON
         if (content instanceof FormData) {
           content.append('access_token', _get());
           $http.post(_getEndpointUri('perform'), content, {
@@ -141,6 +240,17 @@
         return deferred.promise;
       }
       ;
+      /**
+     * Encode embedded JSON
+     *
+     * Not currently using this, but it may be useful soon
+     *
+     * From: http://blog.tryfinally.co.za/2012/12/form-url-encoded-post-with-angularjs.html
+     *
+     * @param string
+     *
+     * @return string
+     */
       function _formEncode(obj) {
         var jsonString = '';
         for (var key in obj) {
@@ -151,14 +261,37 @@
         }
         return jsonString;
       }
+      /**
+     * Get an endpoint URI
+     *
+     * @param string
+     *
+     * @return string
+     */
       function _getEndpointUri(endpoint) {
         return _endpoint.gebo + _endpoint[endpoint];
       }
+      /**
+     * Send a message
+     *
+     * @param Object
+     *
+     * @return promise
+     */
       function _send(message) {
         var deferred = $q.defer();
         message.access_token = _get();
         $http.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
         $http.post(_getEndpointUri('send'), message).success(function (response) {
+          deferred.resolve(response);
+        }).error(function (obj, err) {
+          deferred.reject(err);
+        });
+        return deferred.promise;
+      }
+      function _logout() {
+        var deferred = $q.defer();
+        $http.get(_getEndpointUri('logout')).success(function (response) {
           deferred.resolve(response);
         }).error(function (obj, err) {
           deferred.reject(err);
@@ -178,6 +311,7 @@
         getEndpointUri: _getEndpointUri,
         getParams: _getParams,
         getTokenByPopup: _getTokenByPopup,
+        logout: _logout,
         objectToQueryString: _objectToQueryString,
         perform: _perform,
         verify: _verify,
@@ -195,6 +329,15 @@
     '$scope',
     '$location',
     function ($scope, $location) {
+      /**
+     * Parses an escaped url query string into key-value pairs.
+     *
+     * (Copied from Angular.js in the AngularJS project.)
+     *
+     * @param string
+     *
+     * @returns Object.<(string|boolean)>
+     */
       function parseKeyValue(queryString) {
         var obj = {}, key_value, key;
         angular.forEach((queryString || '').split('&'), function (queryString) {
@@ -207,7 +350,12 @@
         return obj;
       }
       var queryString = $location.path().substring(1);
+      // preceding slash omitted
       var params = parseKeyValue(queryString);
+      // TODO: The target origin should be set to an explicit origin.  Otherwise, 
+      //       a malicious site that can receive the token if it manages to change
+      //       the location of the parent. (See:
+      //       https://developer.mozilla.org/en/docs/DOM/window.postMessage#Security_concerns)
       window.opener.postMessage(params, '*');
       window.close();
     }
